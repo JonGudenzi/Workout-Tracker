eea00363fc51d2b86933639a9a6539d6ffe4ac74
JonGudenzi <jongudenzi@gmail.com>
2021-02-26 18:05:37 -0500

    Creation of fields in workout.js models file

---
+++ models/Workout.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    date: {
+        type: Date,
+        trim: true
+    },
+
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+            },
+
+            name: {
+                type: String,
+                trim: true
+            },
+
+            duration: {
+                type: Number,
+                defuault: 0,
+                allowNull: true
+            },
+
+            weight: {
+                type: Number,
+                default: 0,
+                allowNull: true
+            },
+
+            sets: {
+                type: Number,
+                default: 0,
+                allowNull: true
+            },
+
+            reps: {
+                type: Number,
+                default: 0,
+                allowNull: true
+            },
+
+            distance: {
+                type: Number,
+                default: 0,
+                allowNull: true
+            },
+        }
+
+    ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
